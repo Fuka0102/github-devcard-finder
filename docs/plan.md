6c4397c6399a23450f6d9f16e2c77b75568eaa5f
zumi0102 <mizunofuka0102@gmail.com>
2026-05-30 15:26:13 +0900

    (feat) テキスト入力欄 / 検索ボタンDOM実装

--- docs/plan.md
+++ docs/plan.md
@@ -2,8 +2,8 @@
 
 ## pr1
 ### DOM雑実装
-[ ] テキスト入力欄DOM実装
-[ ] 検索ボタンDOM実装
+[x] テキスト入力欄DOM実装
+[x] 検索ボタンDOM実装
 [ ] inputへ入力したテキストをonChangeで受け取りconsole表示
 [ ] 検索ボタンを押すと、入力したテキストをsubmit
 [ ] Enterキーを押すと、入力したテキストをsubmit
---
+++ src/components/Form.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Form () {
+    return (
+        <>
+            <input type="text" placeholder="input search word"/>
+            <button>search</button>
+        </>
+    );
+}
